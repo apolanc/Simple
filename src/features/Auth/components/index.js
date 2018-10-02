81f30e2e7a4271647c8cdd85e976c1996fb9d6b9
Alexander Polanco <alex20080220@gmail.com>
2018-10-02 18:14:12 -0400

    added new component, prev were corrupted

---
+++ src/features/Auth/components/index.js
@@ -0,0 +1,5 @@
+export { default as LoginForm } from "./LoginForm";
+export { default as RegisterForm } from "./RegisterForm";
+export { default as ForgotPasswordForm } from "./ForgotPasswordForm";
+export { default as AuthFormContent } from "./AuthFormContent";
+export { default as AuthFormContainer } from "./AuthFormContainer";
