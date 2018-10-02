3c8f6eb042e02a33ae9a5840a55999378ff883ac
Alexander Polanco <alex20080220@gmail.com>
2018-10-02 16:56:28 -0400

    test

--- src/features/Auth/Containers/Login.js
+++ src/features/Auth/Containers/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { LoginForm, AuthFormContainer, AuthFormContent } from "../components";
-
+// test
 export default class Login extends Component {
   componentDidCatch() {}
 
