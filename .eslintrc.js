09f174120795f2ab9ce998e5f222d768232874cf
Alexander Polanco <alex20080220@gmail.com>
2018-10-02 15:56:02 -0400

    disabled line break style rule on linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: ["airbnb", "plugin:prettier/recommended"],
   plugins: ["react", "jsx-a11y", "import"],
   rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }]
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "linebreak-style": 0
   },
   env: {
     browser: true
