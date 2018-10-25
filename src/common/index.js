1ed16b2ea3be1066a368ce2f9b02af0fbd64fd2c
Dionisio Ripoll <dripollm@gmail.com>
2018-10-25 15:49:58 -0400

    installed and created datepicker comp

--- src/common/index.js
+++ src/common/index.js
@@ -4,5 +4,6 @@ export { default as PrivateRoute } from "./PrivateRoute";
 export { default as Modal } from "./GenericModal";
 export { default as Slider } from "./Slider";
 export { default as ListItem } from "./list/list-item";
+export { default as DatePicker } from "./DatePicker";
 
 export { Card, Grid, Row, FormGroup };
